32ce842bccee72ea8c0725839325f1d67c7effea
George Karampelas <george.karampelas.26@gmail.com>
2021-01-25 00:54:19 +0200

    another fail try for id3

--- ID3.py
+++ ID3.py
@@ -1,5 +1,7 @@
 import os
 from numpy import log1p as log
+import numpy as np
+import pandas as pd
 
 my_path = 'C:\\Users\\georg\OneDrive - aueb.gr\\artificial_intelligence\\assignment_2\\aclImdb'
 
@@ -64,12 +66,12 @@ def train(myfile):
                 else:
                     dictionary[word] = 1
 
-    fileCount = len(os.listdir(path))
+    # fileCount = len(os.listdir(path))
 
     for word in dictionary.keys():
         dictionary[word]=0
 
-    dictionary["fileCount"] = fileCount
+    # dictionary["fileCount"] = fileCount
 
     for filename in os.listdir(path): #for gia na exei mesa to dictionary
                                         # poses fores emfanizetai sta arxeia
@@ -110,15 +112,21 @@ def train(myfile):
 def entropy(word, pos_dict, neg_dict):
         propability_pos = propability_neg = propability_log_pos = propability_log_neg = 0.0
 
-        if word in pos_dictionary:
-            propability_pos = pos_dictionary[word] / neg_dict[word]+pos_dict[word]
+        if word in pos_dict:
+            if word in neg_dict:
+                propability_pos = pos_dict[word] / neg_dict[word] + pos_dict[word]
+            else:
+                propability_pos = 1
         else:
-            propability_pos = 1 / 25000^2
+            propability_pos = 1 / pow(25000, 2)
 
         if word in neg_dictionary:
-            propability_neg = neg_dictionary[word] / neg_dict[word]+pos_dict[word]
+            if word in pos_dict:
+                propability_neg = neg_dict[word] / neg_dict[word] + pos_dict[word]
+            else:
+                propability_neg = 1
         else:
-            propability_neg = 1 / 25000^2
+            propability_neg = 1 / pow(25000, 2)
 
         propability_log_pos = log(propability_pos)
         propability_log_neg = log(propability_neg)
@@ -133,7 +141,8 @@ def informationGain(word, pos_dict, neg_dict): # legit douleuei
 
     entropia = entropy(word, pos_dict, neg_dict)
 
-    infoGain += 1 -( (neg_dict[word]+pos_dict[word])/25000  * entropia +((12500 -pos_dict[word]) + (12500 -neg_dict[word])/25000) *(1-entropia) )
+    if word in pos_dict.keys() & neg_dict.keys():
+        infoGain += 1 -( (neg_dict[word]+pos_dict[word])/25000  * entropia +((12500 -pos_dict[word]) + (12500 -neg_dict[word])/25000) *(1-entropia) )
 
     return infoGain
 
@@ -164,56 +173,134 @@ def informationGain(word, pos_dict, neg_dict): # legit douleuei
 #
 #
 #         return node
+#
+
+# Second failed try
+# class ID3:
+#     def create(self, pos_dict, neg_dict, all_dict, d):
+#         node = Node(pos_dict, neg_dict)
+#
+#
+#
+#         if len(all_dict) == 0:
+#             return
+#
+#         #find max gain
+#         max = 0
+#         maxWord = None
+#         for word in all_dict.keys():
+#             info = informationGain(word, pos_dict, neg_dict)
+#             if info > max:
+#                 max = info
+#                 maxWord = word
+#
+#         temp_dict = all_dict
+#         del temp_dict[maxWord]
+#
+#         node.value = maxWord
+#
+#
+#     def addNode(self, node, value):
+#         if (node.left == None):
+#             node.left = value
+#             return node
+#         else:
+#             node.right = value
+#             return node
 
 
-class ID3:
-    def create(self, pos_dict, neg_dict, all_dict, d):
-        node = Node(pos_dict, neg_dict)
+def ID3(pos_dict, neg_dict, all_dict, parent = None):
+    if len(all_dict) == 0:
+        return parent
+    elif len(pos_dict)/len_pos <= 0.3: #katw apo 30% toy len toy leksikou me tis 8etikes lekseis return
+        return 1
+    elif len(neg_dict)/len_neg <= 0.3: #katw apo 30% toy len toy leksikou me tis arntikes lekseis return
+        return -1
+    else:
 
 
+        # item_values = [informationGain(word, pos_dict, neg_dict) for word in all_dict.keys()] #ypologise gia all_dict infogain
+        # maxInfo = np.argmax(item_values) #bres to index gia to megalytero IG
 
-        if len(all_dict) == 0:
-            return
+        if len(all_dict) > 0:
+            maxValue = 0
+            maxInfo = "tipota"
+            for key in all_dict:
+                # print(key)
+                temp = informationGain(key, pos_dict, neg_dict)
+                # print(temp)
+                if abs(temp) > maxValue:
+                    maxValue = temp
+                    maxInfo = key
 
-        #find max gain
-        max = 0
-        maxWord = None
-        for word in all_dict.keys():
-            info = informationGain(word, pos_dict, neg_dict)
-            if info > max:
-                max = info
-                maxWord = word
+            # print(maxValue)
+            # print(maxInfo)
+            # print(all_dict)
+            # print(all_dict[maxInfo])
+            best_word = all_dict[maxInfo] #i leksi me to megalutero IG
+            if best_word in pos_dict.keys() & neg_dict.keys():
+                parent_node = 1 if pos_dict[best_word] > neg_dict[best_word] else -1
+            elif best_word not in pos_dict:
+                parent_node = -1
+            else:
+                parent_node = 1
 
-        temp_dict = all_dict
-        del temp_dict[maxWord]
+            # print(all_dict[best_word])
+            all_dict.pop(maxInfo)
+            if best_word in pos_dict:
+                pos_dict.pop(maxInfo)
+            if best_word in neg_dict:
+                neg_dict.pop(maxInfo)
 
-        node.value = maxWord
+            tree = {best_word: {}}
+
+            subtree = ID3(pos_dict, neg_dict, all_dict, parent_node)
+
+            tree[best_word] = subtree
+            print('karkiiiiiiiiiiiiiiiiinossssssss')
+            return tree
+
+def prediction(text, tree, default = 1):
+    for key in list(text.keys()):
+        if key in list(tree.keys()):
+
+            try:
+                result = tree[key][text.key]
+            except:
+                return default
+            if isinstance(result, dict):
+                return prediction(text, result)
+            else:
+                return result
+
+def test(myfile):
+    path = my_path + '\\test\\' + str(myfile)
+    i = 0
+    for filename in os.listdir(path):
+        with open(os.path.sep.join([path, filename]), encoding="utf8") as f:
+            text = f.read()
+            text = clean(text)
+            i += prediction(text, tree, 1)
+    return i
 
 
-    def addNode(self, node, value):
-        if (node.left == None):
-            node.left = value
-            return node
-        else:
-            node.right = value
-            return node
-
-
-def Classify(text):
-    currentNode = root
-    result = None
-    while currentNode.word is not None:
-        result = currentNode.result
-        if currentNode.word in text:
-            currentNode = currentNode.right
-        else:
-            currentNode = currentNode.left
-    return result
+# def Classify(text):
+#     currentNode = root
+#     result = None
+#     while currentNode.word is not None:
+#         result = currentNode.result
+#         if currentNode.word in text:
+#             currentNode = currentNode.right
+#         else:
+#             currentNode = currentNode.left
+#     return result
 
 
 if __name__ == '__main__':
     pos_dictionary = train("pos")
     neg_dictionary = train("neg")
+    len_pos = len(pos_dictionary)
+    len_neg = len(neg_dictionary)
     # all_dict = set()
     # all_dict = pos_dictionary.union(neg_dictionary)
     # for key in pos_dictionary.keys():
@@ -227,12 +314,19 @@ if __name__ == '__main__':
         else:
             all_dict[key] = value
 
-    del all_dict['']
+    # del all_dict['']
+    all_dict.pop('')
 
-    for k in all_dict:
-        print(k)
 
-    root = ID3()
+    print('tree')
+
+    tree = ID3(pos_dictionary, neg_dictionary, all_dict)
+    print(tree)
+    pos = test('pos')
+    neg = test('neg')
+    print ("positive => "+pos)
+    print ("negative => "+neg)
+
 
 
 
